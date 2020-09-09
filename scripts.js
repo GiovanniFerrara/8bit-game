64366b0f88fe1af5ea30ae8e007b7a7ef6474ba1
Giovanni Ferrara <giovanni.ferrara@ergo.digital>
2020-09-09 16:51:45 +0200

    it jumps!

--- scripts.js
+++ scripts.js
@@ -1,39 +1,76 @@
 var ctx = document.getElementById("game").getContext("2d"),
-    canvasTemp = document.createElement("canvas"),
+    backgroundCanvas = document.createElement("canvas"),
     background = new Image(),
     cat = new Image(),
-    tempContext = canvasTemp.getContext("2d"),
+    backgroundCtx = backgroundCanvas.getContext("2d"),
     canvasWidth = 300,
     canvasHeight = 240,
     scrollVal = 0,
-    deltaX = 1
+    deltaX = 1,
+    backgroundData = {},
+    catData = {},
+    gravity = 2,
+    isJumping = false,
+    verticalSpeedAtJump = 10,
+    xStartJump = 0
+    x = 0
+    catCoordinates = {x: 80, y: 105, y0: 105}
 
-    canvasTemp.width = canvasWidth
-    canvasTemp.height = canvasHeight    
-
-    background.src = "bg.jpeg"
-    cat.src = "cat.png"
+    backgroundCanvas.width = canvasWidth
+    backgroundCanvas.height = canvasHeight
+  
     background.onload = loadBackground
 
-    function loadBackground(){
-        tempContext.drawImage(background, 0,0, canvasWidth, canvasHeight)
-        imageData = tempContext.getImageData(0,0,canvasWidth,canvasHeight)
-        render()                
-    }
+    document.onkeydown = checkKey;
 
-    function render(){
-        ctx.clearRect(0,0,canvasWidth,canvasHeight)
-
-        if(scrollVal >= canvasWidth-deltaX){
-            scrollVal = 0
+    function checkKey(e) {
+        e = e || window.event;
+        if (e.keyCode == '38') {
+          isJumping = true
+          xStartJump = x
         }
-
-        scrollVal+=deltaX
-
-        imageData = tempContext.getImageData(canvasWidth-scrollVal, 0, scrollVal, canvasHeight)
-        ctx.putImageData(imageData, 0,0,0,0,scrollVal, canvasWidth)
-        imageData = tempContext.getImageData(0,0,canvasWidth-scrollVal, canvasHeight)
-        ctx.putImageData(imageData, scrollVal,0,0,0,canvasWidth-scrollVal, canvasWidth)
-  
-        requestAnimationFrame(render)
     }
+  
+    function init(){
+      background.src = "bg.jpeg"
+      cat.src = "cat.png"
+      window.requestAnimationFrame(draw);
+    }
+    
+    function loadBackground(){
+      backgroundCtx.drawImage(background, 0,0, canvasWidth, canvasHeight)
+      backgroundData = backgroundCtx.getImageData(0,0,canvasWidth,canvasHeight)
+    }
+
+    function setCatCoordinates(){
+      const t = (x - xStartJump)
+      catCoordinates.y = catCoordinates.y0 - verticalSpeedAtJump * t + 0.5*gravity*t*t
+      if(catCoordinates.y > catCoordinates.y0){
+        catCoordinates.y = catCoordinates.y0
+        isJumping = false
+      }
+    } 
+
+    function draw(){
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+      
+      if(scrollVal >= canvasWidth - deltaX){
+        scrollVal = 0
+      }
+      
+      scrollVal += deltaX
+      x += deltaX
+      
+      backgroundData = backgroundCtx.getImageData(canvasWidth-scrollVal, 0, scrollVal, canvasHeight)
+      
+      isJumping && setCatCoordinates()
+      console.log(isJumping)
+      ctx.putImageData(backgroundData, 0, 0 ,0 ,0 ,scrollVal, canvasWidth)
+      backgroundData = backgroundCtx.getImageData(0,0,canvasWidth-scrollVal, canvasHeight)
+      ctx.putImageData(backgroundData, scrollVal,0,0,0,canvasWidth-scrollVal, canvasWidth)
+      window.requestAnimationFrame(draw);
+
+      ctx.drawImage(cat, catCoordinates.x, catCoordinates.y, 80, 80)
+  }
+
+  init()
